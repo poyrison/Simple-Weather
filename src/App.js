b1c8fca9e0d2793360ac1f0a3955dc7f4dccaf64
김준식 <wnstlr8879@naver.com>
2023-12-14 22:07:00 +0900

    Simple Weather

--- src/App.js
+++ src/App.js
@@ -1,24 +1,191 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+
+const AppWrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  * {
+    box-sizing: border-box;
+  }
+
+  .appContentWrap {
+    background: linear-gradient(180deg, #130754 0%, #3b2f80 100%);
+
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    position: absolute;
+    padding: 5vh 5vw;
+    border-radius: 25px;
+  }
+
+  #inputBox {
+    display: flex;
+    justify-content: center;
+    gap: 14px;
+  }
+
+  #inputBox input {
+    display: flex;
+    font-size: 20px;
+    font-weight: 400;
+    height: 50px;
+    color: #626262;
+    outline: none;
+    border: none;
+    border-radius: 40px;
+    padding-left: 40px;
+  }
+
+  #searchBtn {
+    background: #eee;
+    border-radius: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 50px;
+    height: 50px;
+  }
+
+  #searchBtn:hover {
+    cursor: pointer;
+  }
+
+  #searchBtn:active {
+    background: #fff;
+  }
+
+  #inputBox {
+    display: flex;
+    justify-content: space-around;
+  }
+`;
+
+const ResultWrap = styled.div`
+  text-align: center;
+  margin-top: 60px;
+  padding: 10px;
+  color: #eee;
+
+  .cityData {
+    text-align: center;
+    font-size: 2rem;
+  }
+
+  .temperatureData {
+    text-align: center;
+    font-size: 4rem;
+  }
+
+  .skyData {
+    text-align: center;
+  }
+`;
 
 function App() {
+  const API_KEY = "b438560594f96f5cc956a19915b9fbbe";
+  const [location, setLocation] = useState("");
+  const [result, setResult] = useState({});
+  const [temp, setTemp] = useState(0);
+  const [num, setNum] = useState(0);
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`;
+
+  const convertToF = (celsius) => {
+    let fahrenheit = Math.round((celsius - 273.15) * 10) / 10;
+    setTemp(fahrenheit);
+  };
+
+  const weatherIcon = [
+    "Clear",
+    "Clouds",
+    "Drizzle",
+    "Humidity",
+    "Rain",
+    "Snow",
+    "Wind",
+  ];
+  const weatherIconHandle = (e) => {
+    const weatherIconIndex = weatherIcon.indexOf(e);
+    console.log(weatherIconIndex);
+    console.log(weatherIcon[num]);
+    return weatherIconIndex;
+  };
+
+  const searchWeather = async (e) => {
+    if (e.key === "Enter") {
+      try {
+        await axios.get(url).then((response) => {
+          console.log(response);
+          setResult(response);
+          convertToF(response.data.main.temp);
+          setNum(weatherIconHandle(response.data.weather[0].main));
+        });
+      } catch (err) {
+        alert("도시명을 확인해 주세요");
+      }
+    }
+  };
+  const searchWeatherMouse = async (e) => {
+    try {
+      await axios.get(url).then((response) => {
+        console.log(response);
+        setResult(response);
+        convertToF(response.data.main.temp);
+        setNum(weatherIconHandle(response.data.weather[0].main));
+      });
+    } catch (err) {
+      alert("도시명을 확인해 주세요");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrap>
+      <div className="appContentWrap">
+        <div id="inputBox">
+          <input
+            placeholder="Search"
+            value={location}
+            onChange={(e) => {
+              setLocation(e.target.value);
+            }}
+            type="text"
+            onKeyDown={searchWeather}
+          />
+          <button id="searchBtn" type="button" onClick={searchWeatherMouse}>
+            <img
+              src={process.env.PUBLIC_URL + `/image/Search.png`}
+              alt="검색 버튼 이미지"
+            />
+          </button>
+        </div>
+        {Object.keys(result).length !== 0 && (
+          <ResultWrap>
+            <div>
+              <div className="skyData">
+                <img
+                  id="myImage"
+                  src={
+                    process.env.PUBLIC_URL + `/image/${weatherIcon[num]}.png`
+                  }
+                  width={200}
+                  height={200}
+                  alt={`${result.data.weather[0].main}`}
+                  title={`${result.data.weather[0].main}`}
+                />
+                <div>
+                  <div className="temperatureData">{`${temp}℃`}</div>
+                </div>
+                <div>
+                  <div className="cityData">{`${result.data.name}, ${result.data.sys.country}`}</div>
+                </div>
+              </div>
+            </div>
+          </ResultWrap>
+        )}
+      </div>
+    </AppWrap>
   );
 }
 
